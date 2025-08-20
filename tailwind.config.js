7cdb410023c3d68a1a67be737f1614763bd13382
hoangvu0310 <hoangvu031003@gmail.com>
2025-08-20 09:35:18 +0700

    refactor code base

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {{}} */
+
+const colors = require('./src/constants/colors')
+
+module.exports = {
+  // NOTE: Update this to include the paths to all files that contain Nativewind classes.
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  presets: [require('nativewind/preset')],
+  theme: {
+    extend: {
+      fontFamily: {
+
+      },
+    },
+    colors: { ...colors },
+  },
+  plugins: [],
+}
