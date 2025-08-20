6e6843c9283359372723c2e3d62dc0624223c368
hoangvu0310 <hoangvu031003@gmail.com>
2025-08-20 09:36:37 +0700

    add files

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,14 @@
 const colors = require('./src/constants/colors')
 
 module.exports = {
-  // NOTE: Update this to include the paths to all files that contain Nativewind classes.
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  presets: [require('nativewind/preset')],
-  theme: {
-    extend: {
-      fontFamily: {
-
-      },
-    },
-    colors: { ...colors },
-  },
-  plugins: [],
+	// NOTE: Update this to include the paths to all files that contain Nativewind classes.
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	presets: [require('nativewind/preset')],
+	theme: {
+		extend: {
+			fontFamily: {},
+		},
+		colors: { ...colors },
+	},
+	plugins: [],
 }
